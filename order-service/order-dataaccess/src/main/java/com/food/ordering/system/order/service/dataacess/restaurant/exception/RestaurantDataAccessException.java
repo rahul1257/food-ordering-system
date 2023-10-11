0f55dd7f22e9ec648b7ffe2f639c30ebec986698
Rahul Kumar <rahulkumar1257@gmail.com>
2023-10-11 08:34:54 +0530

    added Order Service Component

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataacess/restaurant/exception/RestaurantDataAccessException.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.order.service.dataacess.restaurant.exception;
+
+public class RestaurantDataAccessException extends RuntimeException {
+
+    public RestaurantDataAccessException(String message) {
+        super(message);
+    }
+}
