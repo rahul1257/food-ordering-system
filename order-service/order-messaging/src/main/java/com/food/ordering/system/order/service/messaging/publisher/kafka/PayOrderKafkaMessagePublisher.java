d72827b8a4b22f4d622900c23200b56e0d468a4e
Rahul Kumar <rahulkumar1257@gmail.com>
2023-10-25 22:46:40 +0530

    added Kafka configurations

---
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/publisher/kafka/PayOrderKafkaMessagePublisher.java
@@ -0,0 +1,30 @@
+package com.food.ordering.system.order.service.messaging.publisher.kafka;
+
+import com.food.ordering.system.kafka.order.avro.model.RestaurantApprovalRequestAvroModel;
+import com.food.ordering.system.kafka.producer.service.KafkaProducer;
+import com.food.ordering.system.order.service.domain.config.OrderServiceConfigData;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+import com.food.ordering.system.order.service.domain.ports.output.message.publisher.restaurantapproval.OrderPaidRestaurantRequestMessagePublisher;
+import com.food.ordering.system.order.service.messaging.mapper.OrderMessagingDataMapper;
+
+public class PayOrderKafkaMessagePublisher implements OrderPaidRestaurantRequestMessagePublisher {
+
+    private final OrderMessagingDataMapper orderMessagingDataMapper;
+    private final OrderServiceConfigData orderServiceConfigData;
+    private final KafkaProducer<String, RestaurantApprovalRequestAvroModel> kafkaProducer;
+
+    public PayOrderKafkaMessagePublisher(OrderMessagingDataMapper orderMessagingDataMapper,
+                                         OrderServiceConfigData orderServiceConfigData,
+                                         KafkaProducer<String, RestaurantApprovalRequestAvroModel> kafkaProducer) {
+        this.orderMessagingDataMapper = orderMessagingDataMapper;
+        this.orderServiceConfigData = orderServiceConfigData;
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @Override
+    public void publish(OrderPaidEvent domainEvent) {
+        String orderId = domainEvent.getOrder().getId().getValue().toString();
+
+
+    }
+}
